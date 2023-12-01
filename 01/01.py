e5180e171bd9aca45ecfa7cdac32553c41e6dcbe
742617000027 <15521878+742617000027@users.noreply.github.com>
2023-12-01 15:15:16 +0100

    refactor day 01

--- 01/01.py
+++ 01/01.py
@@ -1,51 +1,34 @@
 import utils
 
+DIGITS_MAP = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
 
 def get_calibration_value(line, as_letters=False):
     stripped = [c for c in line if c in [str(d) for d in range(10)]]
-    if not as_letters:
-        return int(f'{stripped[0]}{stripped[-1]}')
+    first_real_digit, last_real_digit = stripped[0], stripped[-1]
 
-    d = {
-        'one': '1',
-        'two': '2',
-        'three': '3',
-        'four': '4',
-        'five': '5',
-        'six': '6',
-        'seven': '7',
-        'eight': '8',
-        'nine': '9'
-    }
+    if as_letters:
 
-    first_real_digit = stripped[0]
-    first_real_digit_idx = line.find(first_real_digit)
+        first_real_digit_idx = line.find(first_real_digit)
+        last_real_digit_idx = len(line) - line[::-1].find(last_real_digit) - 1
 
-    last_real_digit = stripped[-1]
-    last_real_digit_idx = len(line) - line[::-1].find(last_real_digit) - 1
+        for k, v in DIGITS_MAP.items():
+            idx = line.find(k, 0, first_real_digit_idx + 1)
+            if idx > -1: first_real_digit, first_real_digit_idx = v, idx
 
-    for k, v in d.items():
-        idx = find_all(k, line)
-        if len(idx) > 0 and idx[0] < first_real_digit_idx:
-            first_real_digit = v
-            first_real_digit_idx = idx[0]
-        if len(idx) > 0 and idx[-1] > last_real_digit_idx:
-            last_real_digit = v
-            last_real_digit_idx = idx[-1]
+            ridx = line.rfind(k, last_real_digit_idx + 1, len(line))
+            if ridx > -1: last_real_digit, last_real_digit_idx = v, ridx
 
     return int(f'{first_real_digit}{last_real_digit}')
-
-
-def find_all(needle, haystack):
-    ret = []
-    idx = 0
-    while True:
-        idx = haystack.find(needle, idx)
-        if idx == -1:
-            break
-        ret.append(idx)
-        idx += len(needle)
-    return ret
 
 
 if __name__ == "__main__":
@@ -59,5 +42,5 @@ if __name__ == "__main__":
     # Part 2
     timer.start()
     print(sum(get_calibration_value(line, as_letters=True) for line in utils.read_str_lines()))
-    timer.stop()  # 37.56ms
+    timer.stop()  # 36.53ms
 
