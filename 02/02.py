da876a4ddab53acf36e9c4141696f5de8ca5f6cd
742617000027 <15521878+742617000027@users.noreply.github.com>
2023-12-02 16:55:15 +0100

    improve conciseness

--- 02/02.py
+++ 02/02.py
@@ -1,5 +1,4 @@
 import re
-from collections import defaultdict
 from functools import reduce
 from operator import mul
 
@@ -8,38 +7,42 @@ import utils
 LIMITS = {'red': 12, 'green': 13, 'blue': 14}
 
 
-def get_values(line):
-    game_part, cubes_part = line.split(': ')
-    game_id = int(game_part.replace('Game ', ''))
+def get_values(i, line):
+    _, cubes_part = line.split(': ')
     quantities = re.findall('(\d+) (blue|green|red)', cubes_part)
-    return game_id if all([int(v) <= LIMITS[color] for v, color in quantities]) else 0, \
-        prod(minimums(quantities).values())
+    return i + 1 if possible(quantities) else 0, power(quantities)
 
 
-def minimums(quantities):
-    d = defaultdict(int)
-    for v, color in quantities:
-        if int(v) > d[color]: d[color] = int(v)
-    return d
+def possible(quantities):
+    return all([int(v) <= LIMITS[color] for v, color in quantities])
+
+
+def power(quantities):
+    return prod([maxval(quantities, color) for color in LIMITS.keys()])
+
+
+def maxval(quantities, color):
+    return max([int(v) for v, c in quantities if c == color])
 
 
 def prod(l):
     return reduce(mul, l, 1)
 
 
+def main():
+    games = utils.read_str_lines()
+    ids, powers = list(zip(*[get_values(i, line) for i, line in enumerate(games)]))
+
+    # Part 1
+    print(sum(ids))
+
+    # Part 2
+    print(sum(powers))
+
+
 if __name__ == "__main__":
     timer = utils.Timer()
 
-    # Part 1
-    # timer.start()
-    # games = utils.read_str_lines()
-    # ids, _ = list(zip(*[get_values(line) for line in games]))
-    # print(sum(ids))
-    # timer.stop()  # 4.12ms
-
-    # Part 2
     timer.start()
-    games = utils.read_str_lines()
-    _, powers = list(zip(*[get_values(line) for line in games]))
-    print(sum(powers))
-    timer.stop()  # 4.22ms
+    main()
+    timer.stop()  # 1.47ms
